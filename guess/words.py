e59779f43f84b552619f86de37b687e31c5eb3c9
mofras <maame-4@hotmail.com>
2024-03-06 16:01:20 +0100

    list of cities

---
+++ guess/words.py
@@ -0,0 +1,6 @@
+class Words:
+    def __init__(self):
+        self.words = ['LONDON', 'ANKARA', 'ABUJA', 'STOCKHOLM', 'OSLO', 'COPENHAGEN', 
+                      'PARIS', 'BERLIN', 'ROME', 'MADRID', 'ATHENS', 'VIENNA', 'BRUSSELS',
+                      'AMSTERDAM']
+        
